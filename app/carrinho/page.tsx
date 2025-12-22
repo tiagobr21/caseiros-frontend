0cb11ab2275397cb57580d549ca389f40f305018
Tiago Souza <tiagobrr17@gmail.com>
2025-12-22 13:56:13 -0400

    add upload image on Create Product

--- app/carrinho/page.tsx
+++ app/carrinho/page.tsx
@@ -323,7 +323,7 @@ export default function CarrinhoPage() {
    
 
           {paymentModal && (
-             <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+             <div className="fixed mt-20 inset-0 bg-black/40 flex items-center justify-center z-50">
               <div className="bg-white rounded-2xl p-6 shadow-xl w-96 relative animate-fade-in">
                 
                        
